5caacf502feae3625a1afbc7eba98882a95608eb
Renemari Padillo <rene@ingenuity.ph>
2016-01-18 12:09:09 +0800

    [CHORE] Minor whitespace fix

--- src/android/BranchSDK.java
+++ src/android/BranchSDK.java
@@ -7,6 +7,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
