260f9f98e1612143b53c0512678f276a65f4dd87
Renemari Padillo <padillo.renemari@gmail.com>
2016-01-28 16:02:24 +0800

    [CHORE] Updated app name for testbed init.sh

--- testbed/init.sh
+++ testbed/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cordova platform add ios android
-cordova plugin add ../ --link --variable BRANCH_LIVE_KEY=key_live_feebgAAhbH9Tv85H5wLQhpdaefiZv5Dv --variable APP_NAME=testapp
+cordova plugin add ../ --link --variable BRANCH_LIVE_KEY=key_live_feebgAAhbH9Tv85H5wLQhpdaefiZv5Dv --variable APP_NAME=testbed
