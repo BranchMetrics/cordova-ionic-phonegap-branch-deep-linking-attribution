8ad7487fdcda66d4b8afd7143f98e718ac856db6
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-28 16:28:12 +0530

    Handled Crash due to empty Context

--- src/android/io/branch/BranchSDK.java
+++ src/android/io/branch/BranchSDK.java
@@ -548,10 +548,12 @@ public class BranchSDK extends CordovaPlugin
 
         this.activity = this.cordova.getActivity();
 
-        Branch debugInstance = Branch.getAutoInstance(this.activity.getApplicationContext());
-
-        if (isEnable) {
-            debugInstance.setDebug();
+        // Handled Random Crash due to empty context
+        if (this.activity != null && this.activity.getApplicationContext() != null) {
+            Branch debugInstance = Branch.getAutoInstance(this.activity.getApplicationContext());
+            if (isEnable) {
+                debugInstance.setDebug();
+            }
         }
 
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, /* send boolean: false as the data */ isEnable));
