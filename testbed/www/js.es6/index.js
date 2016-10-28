be59f7ddf9b3e391bbc5cc8b7fae66445d125dbc
Ethan Neff <ethan.neff@eneff.com>
2016-10-28 16:26:51 -0700

    feat: updated the testbed style and javascript formatting

--- testbed/www/js.es6/index.js
+++ testbed/www/js.es6/index.js
@@ -17,35 +17,17 @@
  * under the License.
  */
 
- var app = {
+var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("deviceready", this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // Event Handlers
     onDeviceReady: function() {
-        app.receivedEvent('app');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-
-        if (navigator.userAgent.indexOf('iPhone') >= 0) {
-            document.getElementsByTagName("html")[0].className = 'ios';
-        }
-        else if (navigator.userAgent.indexOf('Android') >= 0) {
-            document.getElementsByTagName("html")[0].className = 'android';
-        }
-
         // Branch
         SetDebug(true);
         InitSession();
@@ -56,252 +38,266 @@ app.initialize();
 
 function DeepLinkHandler(data) {
     if (data) {
-        alert('Initialize: ' + JSON.stringify(data));
+        alert("Initialize: " + JSON.stringify(data));
     }
     else {
-        alert('No data found');
+        alert("No data found");
     }
 }
 
 function NonBranchLinkHandler(data) {
     if (data) {
-        alert('Non-branch link found: ' + JSON.stringify(data));
+        alert("Non-branch link found: " + JSON.stringify(data));
     }
 }
 
 function InitSession() {
-    console.log('Trigger InitSession()');
+    console.log("Trigger InitSession()");
 
-    Branch.setMixpanelToken('<your-mixpanel-token-here>');
+    Branch.setMixpanelToken("<your-mixpanel-token-here>");
     Branch.initSession().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function SetDebug(isEnabled) {
-    console.log('Trigger SetDebug()');
+    console.log("Trigger SetDebug()");
 
     Branch.setDebug(isEnabled);
-    alert('Debug mode enabled');
+    alert("Debug mode enabled");
 }
 
 function CustomAction() {
-    console.log('Trigger CustomAction()');
+    console.log("Trigger CustomAction()");
 
-    var action = document.getElementById('custom-action').value;
-
-    Branch.userCompletedAction(action).then(function(res) {
+    var action = document.getElementById("custom-action").value;
+    var metadata = { "optional": "dictionary" };
+    Branch.userCompletedAction(action, metadata).then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function GetLatestReferringParams() {
-    console.log('Trigger GetLatestReferringParams()');
+    console.log("Trigger GetLatestReferringParams()");
 
     Branch.getLatestReferringParams().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function GetFirstReferringParams() {
-    console.log('Trigger GetFirstReferringParams()');
+    console.log("Trigger GetFirstReferringParams()");
 
     Branch.getFirstReferringParams().then(function(res) {
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
         console.log(res);
     }).catch(function(err) {
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
         console.error(err);
     });
 }
 
 function SetIdentity() {
-    console.log('Trigger SetIdentity()');
+    console.log("Trigger SetIdentity()");
 
-    var newIdentity = document.getElementById('identity').value;
+    var newIdentity = document.getElementById("identity").value;
 
     Branch.setIdentity(newIdentity).then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function Logout() {
-    console.log('Trigger Logout()');
+    console.log("Trigger Logout()");
 
     Branch.logout().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 var branchUniversalObj = null;
 
 function CreateBranchUniversalObject() {
-
-    console.log('Trigger CreateBranchUniversalObject()');
+    console.log("Trigger CreateBranchUniversalObject()");
 
     var properties = {
-        canonicalIdentifier: 'testbed',
-        title: 'testbed',
-        contentDescription: 'Testbed Application',
-        contentImageUrl: 'https://imgflip.com/s/meme/Derp.jpg',
-        contentIndexingMode: 'public',
+        canonicalIdentifier: "testbed",
+        title: "testbed",
+        contentDescription: "Testbed Application",
+        contentImageUrl: "https://imgflip.com/s/meme/Derp.jpg",
+        contentIndexingMode: "public",
         contentMetadata: {}
     };
 
     Branch.createBranchUniversalObject(properties).then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
         branchUniversalObj = res;
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function RegisterView() {
-    console.log('Trigger RegisterView()');
+    console.log("Trigger RegisterView()");
 
+    if (branchUniversalObj === null) {
+        return alert("need to Generate Branch Universal Object");
+    }
     branchUniversalObj.registerView().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
-    });;
+        alert("Error: " + JSON.stringify(err));
+    });
 }
 
 function GenerateShortUrl() {
-    console.log('Trigger GenerateShortUrl()');
+    console.log("Trigger GenerateShortUrl()");
 
     var properties = {
-        feature: 'test',
-        alias: document.getElementById('alias').value,
-        channel: 'test',
-        stage: 'test',
+        feature: "test",
+        alias: document.getElementById("alias").value,
+        channel: "test",
+        stage: "test",
         duration: 10000
     };
     var controlParams = {
-        $fallback_url: 'www.another.com',
-        $desktop_url: 'www.desktop.com',
-        $android_url: 'test',
-        $ios_url: 'ios',
-        $ipad_url: 'ipad',
-        $fire_url: 'fire',
-        $blackberry_url: 'blackberry',
-        $windows_phone_url: 'win-phone'
+        $fallback_url: "www.another.com",
+        $desktop_url: "www.desktop.com",
+        $android_url: "test",
+        $ios_url: "ios",
+        $ipad_url: "ipad",
+        $fire_url: "fire",
+        $blackberry_url: "blackberry",
+        $windows_phone_url: "win-phone"
     };
 
+    if (branchUniversalObj === null) {
+        return alert("need to Generate Branch Universal Object");
+    }
     branchUniversalObj.generateShortUrl(properties, controlParams).then(function(res) {
         console.log(res);
-        document.getElementById('generated-url').value = res.url;
+        document.getElementById("generated-url").value = res.url;
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function ShowShareSheet() {
-    console.log('Trigger ShowShareSheet()');
+    console.log("Trigger ShowShareSheet()");
 
     var properties = {
-        feature: 'test',
-        alias: document.getElementById('alias').value,
-        channel: 'test',
-        stage: 'test',
+        feature: "test",
+        alias: document.getElementById("alias").value,
+        channel: "test",
+        stage: "test",
         duration: 10000
     };
     var controlParams = {
-        $fallback_url: 'www.another.com',
-        $desktop_url: 'www.desktop.com',
-        $android_url: 'test',
-        $ios_url: 'ios',
-        $ipad_url: 'ipad',
-        $fire_url: 'fire',
-        $blackberry_url: 'blackberry',
-        $windows_phone_url: 'win-phone'
+        $fallback_url: "www.another.com",
+        $desktop_url: "www.desktop.com",
+        $android_url: "test",
+        $ios_url: "ios",
+        $ipad_url: "ipad",
+        $fire_url: "fire",
+        $blackberry_url: "blackberry",
+        $windows_phone_url: "win-phone"
     };
 
     console.log(branchUniversalObj);
 
     // Set listeners
+    if (branchUniversalObj === null) {
+        return alert("need to Generate Branch Universal Object");
+    }
     branchUniversalObj.onShareSheetLaunched(function() {
-        console.log('Share sheet launched');
+        console.log("Share sheet launched");
     });
     branchUniversalObj.onShareSheetDismissed(function() {
-        console.log('Share sheet dismissed');
+        console.log("Share sheet dismissed");
     });
     branchUniversalObj.onLinkShareResponse(function(res) {
-        console.log('Share link response: ' + JSON.stringify(res));
+        console.log("Share link response: " + JSON.stringify(res));
     });
     branchUniversalObj.onChannelSelected(function(res) {
-        console.log('Channel selected: ' + JSON.stringify(res));
+        console.log("Channel selected: " + JSON.stringify(res));
     });
 
-    branchUniversalObj.showShareSheet(properties, controlParams, 'Custom Text');
+    branchUniversalObj.showShareSheet(properties, controlParams, "Custom Text");
 }
 
 function ListOnSpotlight() {
-    console.log('Trigger ListOnSpotlight()');
+    console.log("Trigger ListOnSpotlight()");
+
+        if (branchUniversalObj === null) {
+        return alert("need to Generate Branch Universal Object");
+    }
     branchUniversalObj.listOnSpotlight().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function LoadRewards() {
-    console.log('Trigger LoadRewards()');
+    console.log("Trigger LoadRewards()");
+
     Branch.loadRewards().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function RedeemRewards() {
-    console.log('Trigger RedeemRewards()');
-    var reward = 1000;
+    console.log("Trigger RedeemRewards()");
 
+    var reward = 1000;
     Branch.redeemRewards(reward).then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
 
 function CreditHistory() {
-    console.log('Trigger CreditHistory()');
+    console.log("Trigger CreditHistory()");
+
     Branch.creditHistory().then(function(res) {
         console.log(res);
-        alert('Response: ' + JSON.stringify(res));
+        alert("Response: " + JSON.stringify(res));
     }).catch(function(err) {
         console.error(err);
-        alert('Error: ' + JSON.stringify(err));
+        alert("Error: " + JSON.stringify(err));
     });
 }
