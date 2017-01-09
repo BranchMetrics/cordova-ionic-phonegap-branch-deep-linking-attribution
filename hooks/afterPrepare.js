099d70b439d19e47061fb52e4e7c0b51fa14fb7b
Ethan Neff <ethan.neff@eneff.com>
2017-01-09 15:01:18 -0800

    chore: added android to hook

--- hooks/afterPrepare.js
+++ hooks/afterPrepare.js
@@ -1,5 +1,4 @@
-// read the app/config.xml
-// read the sdk/package.json
+// after prepare hooks based on platform
 (function () {
   // properties
   'use strict'
@@ -8,20 +7,21 @@
   var iosPreferences = require('./lib/ios/xcodePreferences.js')
   var iosAssociatedDomains = require('./lib/ios/associatedDomains.js')
   var iosDevelopmentTeam = require('./lib/ios/developmentTeam.js')
-  // var androidManifest = require('.lib/android/androidManifest.js')
+  var androidManifest = require('./lib/android/androidManifest.js')
   var IOS = 'ios'
   var ANDROID = 'android'
 
   // entry
   module.exports = run
 
+  // methods
   function run (context) {
     var preferences = configPrefrences.read(context)
     var platforms = context.opts.platforms
 
     platforms.forEach(function (platform) {
       if (platform === ANDROID) {
-
+        androidManifest.writePreferences(context, preferences)
       }
       if (platform === IOS) {
         iosPlist.addBranchSettings(preferences)
