6a73e96cda0efab46b53328db62c430054afe3fb
Ethan Neff <ethan.neff@eneff.com>
2017-01-17 18:24:16 -0800

    test: updated docs for android testing

--- testbed/rebuild.sh
+++ testbed/rebuild.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 gulp prod
-npm uninstall mkpath node-version-compare plist xml2js
+# npm uninstall mkpath node-version-compare plist xml2js
 rm -rf ../.installed
 rm -rf ./plugins
 rm -rf ./platforms
 cordova platform add ios
 cordova plugin add ../
-cordova build ios --developmentTeam="PW4Q8885U7"
-open -a Xcode platforms/ios/Branch\ Testing.xcworkspace
+# cordova build ios --developmentTeam="PW4Q8885U7"
+# open -a Xcode platforms/ios/Branch\ Testing.xcworkspace
+
+cordova platform add android
+cordova build android
+open platforms/android/AndroidManifest.xml
