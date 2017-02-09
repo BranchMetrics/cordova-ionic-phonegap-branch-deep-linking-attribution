115484bbd1212570bcf628d2b40ee671ee3c8f0d
Ethan Neff <ethan.neff@eneff.com>
2017-02-09 15:57:50 -0800

    test: updated rebuild.sh for iOS testing

--- testbed/rebuild.sh
+++ testbed/rebuild.sh
@@ -4,22 +4,22 @@
 gulp prod
 
 # clean
-npm install -g cordova
+# npm install -g cordova
 npm uninstall mkpath node-version-compare plist xml2js
 rm -rf ../.installed
 rm -rf ./plugins
 rm -rf ./platforms
 
 # add platforms before plugin because before_plugin_install does not work on file reference
-cordova platform add android
+# cordova platform add android
 cordova platform add ios
 
 # plugin
 cordova plugin add ../
 
 # ios
-cordova run ios --developmentTeam="PW4Q8885U7"
-open -a Xcode platforms/ios/Branch\ Testing.xcworkspace
+cordova run ios --device
+#open -a Xcode platforms/ios/Branch\ Testing.xcworkspace
 
 # android
-cordova run android
+# cordova run android
