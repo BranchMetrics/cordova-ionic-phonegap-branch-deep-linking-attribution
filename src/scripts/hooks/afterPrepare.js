7ae78dd517bea153315549765ba0ceb9e6641b1f
Ethan Neff <ethan.neff@eneff.com>
2017-09-06 15:53:50 -0700

    fix: override singleTop to singleTask on android

--- src/scripts/hooks/afterPrepare.js
+++ src/scripts/hooks/afterPrepare.js
@@ -3,6 +3,8 @@
   'use strict'
   var configPreferences = require('../npm/processConfigXml.js')
   var iosDevelopmentTeam = require('../ios/updateDevelopmentTeam.js')
+  var androidManifest = require('../android/updateAndroidManifest.js')
+  var ANDROID = 'android'
   var IOS = 'ios'
 
   // entry
@@ -14,6 +16,9 @@
     var platforms = context.opts.cordova.platforms
 
     platforms.forEach(function (platform) {
+      if (platform === ANDROID) {
+        androidManifest.writePreferences(context, preferences)
+      }
       if (platform === IOS) {
         iosDevelopmentTeam.addDevelopmentTeam(preferences)
       }
