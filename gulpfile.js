4792400337151cca720e7d7a6353e65b9b831c57
Ethan Neff <ethan.neff@eneff.com>
2018-04-24 08:44:22 -0700

    fix: removed gulp standard linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,86 @@
 // import
-var gulp = require('gulp')
-var fs = require('fs')
-var standard = require('gulp-standard')
+var gulp = require("gulp");
+var fs = require("fs");
 
 // primary tasks
-gulp.task('dev', ['setupDev', 'lint'])
-gulp.task('prod', ['setupNpm', 'lint'])
-gulp.task('lint', ['standard'])
-
-// --------------------------------------------------
-// Linting
-// --------------------------------------------------
-gulp.task('standard', function () {
-  // standard format for javascript
-  var javascript = [
-    './src/**/*.js',
-    './testbed/www/js/**/*.js',
-    './gulpfile.js'
-  ]
-
-  return gulp.src(javascript)
-  .pipe(standard())
-  .pipe(standard.reporter('default', {
-    breakOnError: true,
-    quiet: true
-  }))
-})
+gulp.task("dev", ["setupDev"]);
+gulp.task("prod", ["setupNpm"]);
 
 // --------------------------------------------------
 // Setup
 // --------------------------------------------------
-gulp.task('setupDev', function () {
+gulp.task("setupDev", function() {
   // setup for development use
-  getDevPluginXML()
-  setIosNpmOrDev('dev')
-})
+  getDevPluginXML();
+  setIosNpmOrDev("dev");
+});
 
-gulp.task('setupNpm', function () {
+gulp.task("setupNpm", function() {
   // setup for npm deployment
-  genNpmPluginXML()
-  setIosNpmOrDev('npm')
-})
+  genNpmPluginXML();
+  setIosNpmOrDev("npm");
+});
 
-function getDevPluginXML () {
+function getDevPluginXML() {
   // generate plugin.xml for local development
   // here we reference the frameworks instead of all the files directly
-  var xml = fs.readFileSync('plugin.template.xml', 'utf-8')
+  var xml = fs.readFileSync("plugin.template.xml", "utf-8");
 
-  xml = xml.replace('<!--[Branch Framework Reference]-->', '<framework custom="true" src="src/ios/dependencies/Branch.framework" />')
+  xml = xml.replace(
+    "<!--[Branch Framework Reference]-->",
+    '<framework custom="true" src="src/ios/dependencies/Branch.framework" />'
+  );
 
-  fs.writeFileSync('plugin.xml', xml)
+  fs.writeFileSync("plugin.xml", xml);
 }
 
-function genNpmPluginXML () {
+function genNpmPluginXML() {
   // generate plugin.xml for use as a Cordova plugin
   // here we explode the contents of the frameworks
-  var xml = fs.readFileSync('plugin.template.xml', 'utf-8')
-  var files = []
-  var head = 'src/ios/dependencies/'
-  var newLineIndent = '\n    '
+  var xml = fs.readFileSync("plugin.template.xml", "utf-8");
+  var files = [];
+  var head = "src/ios/dependencies/";
+  var newLineIndent = "\n    ";
 
-  files = files.concat(emitFiles(head + 'Fabric/'))
-  files = files.concat(emitFiles(head + 'Branch-SDK/'))
-  files = files.concat(emitFiles(head + 'Branch-SDK/Networking/'))
-  files = files.concat(emitFiles(head + 'Branch-SDK/Networking/Requests/'))
+  files = files.concat(emitFiles(head + "Fabric/"));
+  files = files.concat(emitFiles(head + "Branch-SDK/"));
+  files = files.concat(emitFiles(head + "Branch-SDK/Networking/"));
+  files = files.concat(emitFiles(head + "Branch-SDK/Networking/Requests/"));
 
-  xml = xml.replace('<!--[Branch Framework Reference]-->', newLineIndent + files.join(newLineIndent))
+  xml = xml.replace(
+    "<!--[Branch Framework Reference]-->",
+    newLineIndent + files.join(newLineIndent)
+  );
 
-  fs.writeFileSync('plugin.xml', xml)
+  fs.writeFileSync("plugin.xml", xml);
 }
 
-function setIosNpmOrDev (npmOrDev) {
-  var content
-  if (npmOrDev === 'npm') {
-    content = '#define BRANCH_NPM true'
-  } else if (npmOrDev === 'dev') {
-    content = '//empty'
+function setIosNpmOrDev(npmOrDev) {
+  var content;
+  if (npmOrDev === "npm") {
+    content = "#define BRANCH_NPM true";
+  } else if (npmOrDev === "dev") {
+    content = "//empty";
   } else {
-    throw new Error('expected deployed|local, not ' + npmOrDev)
+    throw new Error("expected deployed|local, not " + npmOrDev);
   }
-  fs.writeFileSync('src/ios/BranchNPM.h', content + '\n')
+  fs.writeFileSync("src/ios/BranchNPM.h", content + "\n");
 }
 
-function emitFiles (path) {
+function emitFiles(path) {
   // emit array of Cordova file references for all .h/.m files in path
-  var ret = []
+  var ret = [];
   for (var filename of fs.readdirSync(path)) {
-    var fileType = null
+    var fileType = null;
     if (filename.match(/\.m$/)) {
-      fileType = 'source'
+      fileType = "source";
     } else if (filename.match(/\.h$/) || filename.match(/\.pch$/)) {
-      fileType = 'header'
+      fileType = "header";
     }
     if (fileType) {
-      ret.push('<' + fileType + '-file src="' + path + filename + '" />')
+      ret.push("<" + fileType + '-file src="' + path + filename + '" />');
     }
   }
-  ret.push('')
-  return ret
+  ret.push("");
+  return ret;
 }
