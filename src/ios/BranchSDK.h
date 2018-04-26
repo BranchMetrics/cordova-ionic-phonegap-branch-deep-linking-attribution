043ca8453bda5f9f97abd0d63bda7bce13ff3a14
Ethan Neff <ethan.neff@eneff.com>
2018-04-25 17:07:57 -0700

    fix: removed mixpanel token

--- src/ios/BranchSDK.h
+++ src/ios/BranchSDK.h
@@ -27,7 +27,6 @@
 
 // BranchSDK Basic Methods
 - (void)initSession:(CDVInvokedUrlCommand*)command;
-- (void)setMixpanelToken:(CDVInvokedUrlCommand*)command;
 - (void)disableTracking:(CDVInvokedUrlCommand*)command;
 - (void)setDebug:(CDVInvokedUrlCommand*)command;
 - (void)getAutoInstance:(CDVInvokedUrlCommand*)command;
